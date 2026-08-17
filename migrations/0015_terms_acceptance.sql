3b0d96cbeff94f3ece30476c0b5671f722c82375
akzinn <irja@simnet.is>
2026-08-17 11:03:55 +0000

    fix(terms): correct migration header comment

--- migrations/0015_terms_acceptance.sql
+++ migrations/0015_terms_acceptance.sql
@@ -1,3 +1,3 @@
--- migrations/0009_terms_acceptance.sql
+-- migrations/0015_terms_acceptance.sql
 ALTER TABLE orders ADD COLUMN terms_accepted_at TEXT;
 ALTER TABLE orders ADD COLUMN terms_version TEXT;
